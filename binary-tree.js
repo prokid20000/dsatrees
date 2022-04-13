80a652c3b55678be48dc6d99cd0a6fa1318dbc60
brendan <lobrendan@gmail.com>
2022-04-13 11:40:39 -0700

    working on binary-tree nextLarger

--- binary-tree.js
+++ binary-tree.js
@@ -17,21 +17,30 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth(node = this.root) {
+    console.log("minDepth, node :", node);
+    if (!node) return 0;
 
+    return Math.min(this.minDepth(node.left), this.minDepth(node.right)) + 1;        
   }
+  
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
   maxDepth(node = this.root) {
+    console.log("maxDepth, node :", node);
+    if (!node) return 0;
 
+    return Math.max(this.maxDepth(node.left), this.maxDepth(node.right)) + 1;        
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound, node = this.root) {
+    if (!node) return 0;
 
+    if (node.val > lowerBound)
   }
 
   /** Further study!
