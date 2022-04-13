c1f055f4c700fc81fb45fff5e172cf60c2aa93a2
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-04-13 14:18:51 -0400

    Completed normal trees

--- tree.js
+++ tree.js
@@ -15,19 +15,61 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    if (this.root === null) return 0;
+    let numbersStack = [this.root];
+
+    let sum = 0;
+
+    while (numbersStack.length) {
+      let current = numbersStack.pop();
+      sum += current.val;
+
+      for (let child of current.children) {
+        numbersStack.push(child);
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (this.root === null) return 0;
+    let numbersStack = [this.root];
+    let countEven = 0;
 
+    while (numbersStack.length) {
+      let current = numbersStack.pop();
+      if (current.val % 2 === 0) {
+        countEven += 1;
+      }
+
+      for (let child of current.children){
+        numbersStack.push(child);
+      }
+    }
+    return countEven;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (this.root === null) return 0;
+    let numbersStack = [this.root];
+    let countGreater = 0;
+
+    while (numbersStack.length) {
+      let current = numbersStack.pop();
+      if (current.val > lowerBound) {
+        countGreater += 1;
+      }
+
+      for (let child of current.children){
+        numbersStack.push(child);
+      }
+    }
+    return countGreater;
 
   }
 }
