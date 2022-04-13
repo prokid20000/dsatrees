b1043e0db021b2b8b254e61f79320ac735fd63dd
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-04-13 15:31:04 -0400

    WIP on areCousins

--- binary-tree.js
+++ binary-tree.js
@@ -17,7 +17,7 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth(node = this.root) {
-    console.log("minDepth, node :", node);
+    // console.log("minDepth, node :", node);
     if (!node) return 0;
 
     return Math.min(this.minDepth(node.left), this.minDepth(node.right)) + 1;        
@@ -28,7 +28,7 @@ class BinaryTree {
    * the length of the longest path from the root to a leaf. */
 
   maxDepth(node = this.root) {
-    console.log("maxDepth, node :", node);
+    // console.log("maxDepth, node :", node);
     if (!node) return 0;
 
     return Math.max(this.maxDepth(node.left), this.maxDepth(node.right)) + 1;        
@@ -38,9 +38,21 @@ class BinaryTree {
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound, node = this.root) {
-    if (!node) return 0;
+    if (!node) return null;
+    let numberStack = [node];
+    let nextLowest = Infinity;
 
-    if (node.val > lowerBound)
+    while(numberStack.length){
+      let current = numberStack.pop();
+
+      if (current.val > lowerBound && current.val < nextLowest){
+        nextLowest = current.val;
+      }
+      if(current.left) numberStack.push(current.left);
+      if(current.right) numberStack.push(current.right);
+    }
+
+    return nextLowest === Infinity ? null : nextLowest;
   }
 
   /** Further study!
@@ -48,6 +60,38 @@ class BinaryTree {
    * (i.e. are at the same level but have different parents. ) */
 
   areCousins(node1, node2) {
+    function depth(nodeToSearch, node= this.root){
+      if (!node) return -1;
+
+      if(node === nodeToSearch) return 0;
+
+      if(node.left){
+        let leftSide = depth(node.left);
+        return leftSide === -1 ? -1 : leftSide + 1;
+      }
+      if(node.right){
+        let rightSide = depth(node.right);
+        return rightSide === -1 ? -1: rightSide + 1;
+      } 
+    }
+
+    function parent(nodeToSearch, node= this.root){
+      if (!node) return null;
+      let nodeStack = [node];
+      while(nodeStack.length){
+        let current = nodeStack.pop();
+        if (current.left === nodeToSearch || current.right === nodeToSearch){
+          return current;
+        }
+        if(current.left) nodeStack.push(current.left);
+        if(current.right) nodeStack.push(current.right);
+      }
+      return null;
+    }
+    if(depth(node1) === depth(node2) && parent(node1) !== parent(node2)){
+      return true;
+    }
+    return false;
   }
 
 }
